8944808c2432f99b80442e1b56a462cbc4eccdf8
Anthony <ajorona@gmail.com>
2017-03-16 22:55:01 -0700

    updated ajax methods

--- js/test.js
+++ js/test.js
@@ -1,64 +1,61 @@
-    var getWeather = function(countyObj) {
+//jQuery AJAX requests return promise object
+var getWeather = function (obj) {
         return $.ajax({
             dataType: "json",
             method: "GET",
             url: "http://api.wunderground.com/api/" +
                 "d406854e7b14e2ec" + "/conditions/q/CA/" +
-                countyObj.position + ".json"
+                obj.position + ".json"
         });
     };
-    var getWiki = function(countyObj) {
+
+var getWiki = function (obj) {
         return $.ajax({
             url: "http://en.wikipedia.org/w/api.php" +
                 "?origin=*" + "&format=json" +
                 "&action=query" + "&prop=extracts" +
                 "&exintro=" + "&explaintext=" + "&titles=" +
-                countyObj.position,
+                obj.position,
             method: 'GET'
         });
     };
-    var getNYT = function(countyObj) {
-        var url =
-            "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-        url += '?' + $.param({
-            'api-key': "878139c3c6844fac94b815b3a4698c03",
-            'q': countyObj.position,
-            'sort': 'newest',
-            'page': 0
-        });
-        return $.ajax({
-            url: url,
-            method: 'GET'
-        });
-    };
-    var CountyData = function(name) {
-        this.weather = {};
-        this.wiki = {};
-        this.nyt = {};
-        this.name = name;
-        var currentCounty = new CountyData();
-        var getCounty = function(name) {
-            var countyObj = countyDatabase[name];
-            $.when(getWeather(countyObj), getNYT(countyObj),
-                getWiki(countyObj)).then(function(weather,
-                nyt, wiki) {
-                currentCounty.weather = weather;
-                currentCounty.nyt = nyt;
-                currentCounty.wiki = wiki;
-                currentCounty.name = countyObj.name;
-            });
-        return currentCounty
-        };
-        
-    var CountyData = function (name) {
-        this.weather = {};
-        this.wiki = {};
-        this.nyt = {};
-        this.name = name;
-    };
-    
-        
-    currentCounty = CountyData('alameda');
-    console.log(currentCounty);
-        
-        
+var getNYT = function (obj) {
+    var url =
+        "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "878139c3c6844fac94b815b3a4698c03",
+        'q': obj.position,
+        'sort': 'newest',
+        'page': 0
+    });
+    return $.ajax({
+        url: url,
+        method: 'GET'
+    });
+};
+
+var CountyAPI = function (county, site) {
+    this.county = county.position;
+    this.site = site.position;
+    this.weather = {};
+    this.wiki = {}; 
+    this.nyt = {};
+};
+
+var getAPI = function(name) {
+    var county = countyDatabase[name];
+    var site = SViewDatabase[name + 'site'];
+    var countyAPI = new CountyAPI(county, site);
+    $.when(getWeather(county), getNYT(county),
+        getWiki(site)).then(function (weather,
+        nyt, wiki) {
+        countyAPI.weather = weather;
+        countyAPI.nyt = nyt;
+        countyAPI.wiki = wiki;
+    });
+    return countyAPI;
+};
+
+//external raw git file used for countyDatabase
+    var current = getAPI('alameda');
+    console.log(current);
